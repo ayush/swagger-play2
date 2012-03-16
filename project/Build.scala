f13c940de4a302e6d2ece73c95316793bfcd4bd6
Jieren Chen <jieren.chen@gmail.com>
2012-03-16 15:16:55 -0700

    fixes to url and httpmethod problems with Play2

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "swagger-play2"
-    val appVersion      = "1.0"
+    val appVersion      = "1.0.1"
 
     val appDependencies = Seq(
 	  "org.codehaus.jackson" % "jackson-jaxrs" % "1.7.1",
@@ -21,12 +21,10 @@ object ApplicationBuild extends Build {
 	val wnRepo = Some(Resolver.url("wordnik-remote-repos", new URL( "https://ci.aws.wordnik.com/artifactory/libs-snapshots"))(Resolver.ivyStylePatterns))
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-    // val main = PlayProject(appName, appVersion, appDependencies).settings(
 	 	resolvers += "local-maven-repo" at "file://" + Path.userHome.absolutePath + "/.m2/repository/",
 	 	resolvers += Resolver.url("local-ivy", new URL( "file://" + Path.userHome.absolutePath + "/.ivy2/local"))(Resolver.ivyStylePatterns),
 	 	resolvers += Resolver.url("local-ivy-cache", new URL( "file://" + Path.userHome.absolutePath + "/.ivy2/cache"))(Resolver.ivyStylePatterns),
 	 	resolvers += Resolver.url("wordnik-remote-repos", new URL( "https://ci.aws.wordnik.com/artifactory/libs-snapshots"))(Resolver.ivyStylePatterns),
-	 	resolvers += "java-net" at "http://download.java.net/maven/2",
-        publishTo := wnRepo
+	 	resolvers += "java-net" at "http://download.java.net/maven/2"
         )
 }
