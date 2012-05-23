4be93d25fbe7ba5ceda1ef79ee9a193a45b00b40
Jieren Chen <jieren.chen@gmail.com>
2012-05-23 15:19:36 -0700

    Do not publish routes

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "swagger-play2"
-    val appVersion      = "1.0.1"
+    val appVersion      = "1.0.2-rc"
 
     val appDependencies = Seq(
 	  "org.codehaus.jackson" % "jackson-jaxrs" % "1.7.1",
@@ -21,6 +21,30 @@ object ApplicationBuild extends Build {
 	val wnRepo = Some(Resolver.url("wordnik-remote-repos", new URL( "https://ci.aws.wordnik.com/artifactory/libs-snapshots"))(Resolver.ivyStylePatterns))
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+        // Never publish routes or application.conf
+        mappings in (Compile,packageBin) ~= { (ms: Seq[(File, String)]) =>
+            val routesFilter = """Routes\$\$anonfun\$routes\$[0-9]+\$\$anonfun\$apply\$([\w\$]+).class""".r
+            val mainRoutesFilter = """Routes\$\$anonfun\$routes\$([0-9]+).class""".r
+            val routesStraightFilter = """(Routes\$.class)""".r
+            val controllerRoutesFilter = """([\w\$/]*)controllers/routes([\w\$]*).class""".r
+            ms filter {                
+                case (file, toPath) => {
+                    println(file.toString)
+                    println(toPath)
+                    val routesMatch = toPath match {
+                        case routesFilter(_) => true
+                        case mainRoutesFilter(_) => true
+                        case routesStraightFilter(_) => true
+                        case controllerRoutesFilter(_,_) => true
+                        case _ => false }
+                    !routesMatch && toPath != "routes" && toPath != "application.conf"
+                }
+            }
+        },
+        
+        credentials := Credentials(Path.userHome / ".ivy2" / ".credentials") :: Nil,
+        publishTo := Some("kloutPluginReleases" at "http://maven-repo:8081/artifactory/ext-release-local"),
+
 	 	resolvers += "local-maven-repo" at "file://" + Path.userHome.absolutePath + "/.m2/repository/",
 	 	resolvers += Resolver.url("local-ivy", new URL( "file://" + Path.userHome.absolutePath + "/.ivy2/local"))(Resolver.ivyStylePatterns),
 	 	resolvers += Resolver.url("local-ivy-cache", new URL( "file://" + Path.userHome.absolutePath + "/.ivy2/cache"))(Resolver.ivyStylePatterns),
